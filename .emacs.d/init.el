657fd79f07062c2736ff76ec21af0460b8611ebf
AxelSilverdew <7677954+AxelSilverdew@users.noreply.github.com>
2017-12-26 17:02:34 +0530

    Emacs update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -117,6 +117,13 @@
 (use-package 
   try)
 
+;; Emmet Mode
+(use-package
+  emmet-mode)
+
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+
 ;; Shows a nice and friendly panel with possible completions of your failed shortcut attemps
 (use-package 
   which-key 
